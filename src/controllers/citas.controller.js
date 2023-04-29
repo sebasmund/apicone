ca9beb32fab13197970f49b3abe54362c7301c0a
sebasmund <sebasmundaray@gmail.com>
2023-04-29 16:26:08 -0500

    Creación controller citas

---
+++ src/controllers/citas.controller.js
@@ -0,0 +1,81 @@
+import { getConnection } from "./../database/database";
+
+const getCitas = async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id_cita, id_paciente, id_dentista, fecha_hora FROM citas");
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const getCita = async (req, res) => {
+    try {
+        const { id_cita } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id_cita, id_paciente, id_dentista, fecha_hora WHERE id_cita = ?", id_cita);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const addCita = async (req, res) => {
+    try {
+        const { id_paciente, id_dentista, fecha_hora } = req.body;
+
+        if (id_paciente === undefined || id_dentista === undefined || fecha_hora === undefined) {
+            res.status(400).json({ message: "Solicitud invalida. Rellene todos los campos." });
+        }
+
+        const cita = { id_paciente, id_dentista, fecha_hora };
+        const connection = await getConnection();
+        await connection.query("INSERT INTO citas SET ?", cita);
+        res.json({ message: "Cita añadida de manera exitosa" });
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const updateCita = async (req, res) => {
+    try {
+        const { id_cita } = req.params;
+        const { id_paciente, id_dentista, fecha_hora } = req.body;
+
+        if (id_cita === undefined || id_paciente === undefined || id_dentista === undefined || fecha_hora === undefined) {
+            res.status(400).json({ message: "Solicitud invalida. Rellene todos los campos." });
+        }
+
+        const cita = { id_paciente, id_dentista, fecha_hora };
+        const connection = await getConnection();
+        const result = await connection.query("UPDATE citas SET ? WHERE id_cita = ?", [cita, id_cita]);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+const deleteCita = async (req, res) => {
+    try {
+        const { id_cita } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("DELETE FROM citas WHERE id = ?", id_cita);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+
+export const methods = {
+    getCitas,
+    getCita,
+    addCita,
+    updateCita,
+    deleteCita
+};
